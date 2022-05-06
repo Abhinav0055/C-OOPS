12c27d50d428ef4dafe082fa883f81cfb2cd3a6a
Abhinav0055 <abhinav0055.be21@chitkara.edu.in>
2022-05-06 15:44:01 +0530

    commit

---
+++ Operator Overloading/Matrix Add Multiplication.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+
+using namespace std;
+
+class matrix{
+    int mat[3][3];
+    public:
+    void set(){
+        int i,j;
+        for(i=0;i<=2;i++){
+            for(j=0;j<=2;j++){
+                cin>>mat[i][j];
+            }
+        }
+        
+    }
+    
+    matrix operator+(matrix m2){
+        int i,j;
+        matrix m3;
+        for(i=0;i<=2;i++){
+            for(j=0;j<=2;j++){
+                m3.mat[i][j]=mat[i][j]+m2.mat[i][j];
+            }
+        }
+        return m3;
+    }
+     matrix operator*(matrix m2){
+        int i,j;
+        matrix m3;
+        for(i=0;i<=2;i++){
+            for(j=0;j<=2;j++){
+                m3.mat[i][j]=mat[i][j]*m2.mat[i][j];
+            }
+        }
+        return m3;
+    }
+     matrix operator-(matrix m3){
+        int i,j;
+        matrix m4;
+        for(i=0;i<=2;i++){
+            for(j=0;j<=2;j++){
+                m4.mat[i][j]=mat[i][j]-m3.mat[i][j];
+            }
+        }
+        return m4;
+    }
+    void disp(){
+        int i,j;
+        for(i=0;i<=2;i++){
+            for(j=0;j<=2;j++){
+                cout<<mat[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+        
+        
+        
+    }
+};
+
+
+
+int main()
+{
+    matrix m1,m2,m3,m4;
+    m1.set();
+    m2.set();
+    //m3=m1+m2;
+    m4=m1-m1*m2;
+    m4.disp();
+
+    return 0;
+}
